ccadb0cf40c447efa8c064ffd1960d5883341237
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-09-13 16:44:24 +0300

    constructor pattern

---
+++ constructorPattern.js
@@ -0,0 +1,63 @@
+// a constructor is a special function in a class
+// which initializes an object with some set of default and/or sent-in values
+
+// create an object
+
+let instance = {};
+
+let instance = Object.create(Object.prototype);
+
+let instance = new Object();
+
+// add properties to the object
+
+// the dot notation
+instance.key = "Value";
+
+// the bracket notation
+instance["key"] = "Value";
+
+// setting a single property
+Object.defineProperty(instance, "key", {
+  value: "Value",
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
+// setting multiple properties
+Object.defineProperties(instance, {
+  "firstKey": {
+    value: "First key's value",
+    writable: true
+  },
+  "secondKey": {
+    value: "Second key's value",
+    writable: false
+  }
+});
+
+// defines a constructor for Person objects
+function Person(name, age, isDeveloper) {
+  this.name = name;
+  this.age = age;
+  this.isDeveloper = isDeveloper || false;
+}
+
+Person.prototype.killsBugs = function() {
+  console.log(this.isDeveloper
+    ? "This person does kill bugs"
+    : "This person does not kill bugs");
+}
+
+// creates a Person instance with properties
+// name: Dilyana, age: 28, isDeveloper: true and a method killsBugs
+var person1 = new Person("Dilyana", 28, true);
+// creates a Person instance with properties
+// name: Linus, age: 32, isDeveloper: false and a method killsBugs
+var person2 = new Person("Linus", 32);
+
+// prints out: This person does write code
+person1.killsBugs();
+// prints out: this person does not write code
+person2.killsBugs();
